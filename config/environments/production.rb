06a8c5d26f9200f34a4d9af372109b8bd4a996e7
Ubuntu <ubuntu@ip-172-31-10-118.ap-south-1.compute.internal>
2017-07-21 04:28:29 +0000

    before god push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,22 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    # :user_name            => ENV['gmail_username'],
+    # :password             => ENV['gmail_password'],
+    :user_name            => 'votemeindia@gmail.com',
+    :password             => '18101996good',
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.perform_deliveries = true
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
